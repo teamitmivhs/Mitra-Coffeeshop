bae1072df67692cbcb66cff37738878786cab37a
parothegreat <alvaroprayogo38@gmail.com>
2026-06-12 05:43:37 +0700

    [fix] add favicon generated from logo.jpg

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -93,6 +93,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       { rel: "stylesheet", href: appCss },
+      { rel: "icon", href: "/favicon.ico" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
